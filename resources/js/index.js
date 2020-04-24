87682101cf078558274b1dfd4f11fa43d1dc4c8f
Acromyon <doc.domrachev@mail.ru>
2020-04-24 10:40:45 +0300

    made switch between pages; exchange CDN libs to local;

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,8 @@
+import $ from 'jquery';
 import '../scss/common.scss';
+
+$(document).on('click', '.js-turn-page', () => {
+    let pages = $('.js-page');
+    
+    pages.toggleClass('js-current_page');
+});
