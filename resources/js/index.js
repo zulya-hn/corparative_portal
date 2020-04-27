7728b0128276f164792c21783c6c2dcb659ae0cd
Acromyon <doc.domrachev@mail.ru>
2020-04-27 12:09:51 +0300

    improved modal popup styles;

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import '../scss/common.scss';
 
-$(document).on('click', '.js-turn-page', () => {
+$(document).on('click', '.js-turn-page, #alert-ok', () => {
     let pages = $('.js-page');
     
     pages.toggleClass('js-current_page');
